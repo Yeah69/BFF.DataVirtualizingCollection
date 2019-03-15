2003e7dfe5cf6b7c423b22b88ea4176fce7cfc0b
Dima Enns <dieter.enns@gmail.com>
2019-03-15 22:55:55 +0100

    #1: Sample as requested by issam1975
    https://github.com/Yeah69/BFF.DataVirtualizingCollection/issues/1

---
+++ BFF.DataVirtualizingCollection.Sample/Converters.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace BFF.DataVirtualizingCollection.Sample
+{
+    public static class Converters
+    {
+        public static IValueConverter BoolToVisibility =
+            LambdaConverters.ValueConverter.Create<bool, Visibility>(
+                e => e.Value ? Visibility.Visible : Visibility.Collapsed);
+    }
+}
--- BFF.DataVirtualizingCollection.Sample/MainWindow.xaml.cs
+++ BFF.DataVirtualizingCollection.Sample/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using MahApps.Metro.IconPacks;
 using Microsoft.Data.Sqlite;
 
 namespace BFF.DataVirtualizingCollection.Sample
@@ -104,6 +109,161 @@ namespace BFF.DataVirtualizingCollection.Sample
                 })
             .SyncIndexAccess();
 
+        public IList<ProfileViewModel> Profiles { get; } = DataVirtualizingCollectionBuilder<ProfileViewModel>
+            .Build(12)
+            .Hoarding()
+            .NonPreloading()
+            .NonTaskBasedFetchers(
+                (offset, pageSize) =>
+                {
+                    Console.WriteLine($"{nameof(Profiles)}: Loading page with offset {offset}");
+                    return Enumerable.Range(offset, pageSize).Select(i => ProfilePool[i % ProfilePool.Count]).ToArray();
+                },
+                () =>
+                {
+                    Console.WriteLine($"{nameof(Profiles)}: Loading count");
+                    return 420420;
+                })
+            .SyncIndexAccess();
+
+        private static IReadOnlyList<ProfileViewModel> ProfilePool { get; } =
+            new ReadOnlyCollection<ProfileViewModel>(
+                new List<ProfileViewModel>
+                {
+                    new ProfileViewModel(
+                        "UI/UX designer",
+                        "$55/hr", 
+                        "Wide Walson", 
+                        "Wade is a 32 year old UI/UX designer, with an impressive portfolio behind him.", 
+                        true,
+                        false, 
+                        "Epic Coders",
+                        new List<string>{ "UI", "UX", "photoshop" },
+                        4,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/00_Wide.png"))),
+                    new ProfileViewModel(
+                        "mobile designer",
+                        "$32/hr",
+                        "Paria Metrescu",
+                        "Paria is an android and iOS developer who worked at Apple for 6 years.",
+                        false,
+                        true,
+                        null,
+                        new List<string>{ "PHP", "android", "iOS"},
+                        2,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/01_Paria.png"))),
+                    new ProfileViewModel(
+                        "mobile designer",
+                        "$42/hr",
+                        "Morexandra Algan",
+                        "Morexandra is a dedicated developer for mobile platforms and is very good at it.",
+                        false,
+                        true,
+                        null,
+                        new List<string>{ "PHP", "android", "iOS"},
+                        12,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/02_Morexandra.png"))),
+                    new ProfileViewModel(
+                        "interactive designer",
+                        "$44/hr",
+                        "Smennifer Jith",
+                        "Smennifer is an interactive designer who is really awesome at what she does.",
+                        false,
+                        true,
+                        null,
+                        new List<string>{ "PHP", "android", "iOS"},
+                        2,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/03_Smennifer.png"))),
+                    new ProfileViewModel(
+                        "mobile designer",
+                        "$40/hr",
+                        "Anyetlana Svukova",
+                        "Anyetlana is an Android and iOS designer with advanced knowledge in coding.",
+                        true,
+                        true,
+                        null,
+                        new List<string>{ "PHP", "android", "iOS"},
+                        2,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/04_Anyetlana.png"))),
+                    new ProfileViewModel(
+                        "UI/UX designer",
+                        "$30/hr",
+                        "Korko van Maoh",
+                        "Korko is a 25 year old web designer with an impressive portfolio behind him.",
+                        false,
+                        false,
+                        "Visual Madness",
+                        new List<string>{ "UI", "UX", "photoshop"},
+                        4,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/05_Korko.png"))),
+                    new ProfileViewModel(
+                        "UX designer",
+                        "$50/hr",
+                        "Kowel Paszentka",
+                        "Kowel is a 32 year old UX designer, with over 10 years of experience in what he does.",
+                        false,
+                        false,
+                        "Apple Inc",
+                        new List<string>{ "UI", "UX", "photoshop" },
+                        4,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/06_Kowel.png"))),
+                    new ProfileViewModel(
+                        "mobile designer",
+                        "$32/hr",
+                        "Sinia Samionov",
+                        "Sinia is an android and iOS developer who worked at Apple for 6 years.",
+                        false,
+                        true,
+                        null,
+                        new List<string>{ "PHP", "android", "iOS" },
+                        2,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/07_Sinia.png"))),
+                    new ProfileViewModel(
+                        "photographer",
+                        "$40/hr",
+                        "Wonathan Jayne",
+                        "Wonathan is a 28 year old photographer from London with real talent for what he does.",
+                        false,
+                        false,
+                        "Epic Coders",
+                        new List<string>{ "UI", "UX", "photoshop" },
+                        4,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/08_Wonathan.png"))),
+                    new ProfileViewModel(
+                        "Superhero",
+                        "free",
+                        "Matban",
+                        "I'm Matban!",
+                        false,
+                        true,
+                        null,
+                        new List<string>{ "tech", "IT", "martial arts" },
+                        69,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/09_Matban.png"))),
+                    new ProfileViewModel(
+                        "mobile designer",
+                        "$39/hr",
+                        "Surgiana Geoclea",
+                        "Surgiana is an android and iOS developer who worked at Apple for 6 years.",
+                        false,
+                        true,
+                        null,
+                        new List<string>{ "PHP", "android", "iOS" },
+                        2,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/10_Surgiana.png"))),
+                    new ProfileViewModel(
+                        "UI/UX designer",
+                        "$45/hr",
+                        "Jogory Grehnes",
+                        "Jogory is a 32 year old UI/UX designer, with an impressive portfolio behind him.",
+                        false,
+                        false,
+                        "Epic Coders",
+                        new List<string>{ "UI", "UX", "photoshop" },
+                        4,
+                        new BitmapImage(new Uri("pack://application:,,,/ProfilePics/11_Jogory.png")))
+                });
+
     }
 
     public class MyObject
@@ -117,5 +277,72 @@ namespace BFF.DataVirtualizingCollection.Sample
         }
 
         public int Number { get; }
+    }
+
+    public class ProfileViewModel
+    {
+        public static IValueConverter ToCompanyBrush =
+            LambdaConverters.ValueConverter.Create<ProfileViewModel, Brush>(
+                e => e.Value.IsFreelancer ? Brushes.Green : Brushes.Blue);
+
+        public static IValueConverter ToCompanyText =
+            LambdaConverters.ValueConverter.Create<ProfileViewModel, string>(
+                e => e.Value.IsFreelancer ? "Freelancer" : e.Value.CompanyName);
+
+        public static IValueConverter ToCompanyIcon =
+            LambdaConverters.ValueConverter.Create<ProfileViewModel, PackIconMaterialKind>(
+                e => e.Value.IsFreelancer ? PackIconMaterialKind.AccountOutline : PackIconMaterialKind.City);
+
+        public static IValueConverter PrefixedHiddenAbilitiesCount =
+            LambdaConverters.ValueConverter.Create<int, string>(
+                e => $"+{e.Value}");
+
+        public static IValueConverter ProfilesTitle =
+            LambdaConverters.ValueConverter.Create<int, string>(
+                e => $"Profiles ({e.Value})");
+
+        public ProfileViewModel(
+            string occupation, 
+            string salary,
+            string name, 
+            string description,
+            bool isAvailable,
+            bool isFreelancer,
+            string companyName,
+            IReadOnlyList<string> abilities,
+            int hiddenAbilitiesCount,
+            ImageSource picture)
+        {
+            Occupation = occupation;
+            Salary = salary;
+            Name = name;
+            Description = description;
+            IsAvailable = isAvailable;
+            IsFreelancer = isFreelancer;
+            CompanyName = companyName;
+            Abilities = abilities;
+            HiddenAbilitiesCount = hiddenAbilitiesCount;
+            Picture = picture;
+        }
+
+        public string Occupation { get; }
+
+        public string Salary { get; }
+
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public bool IsAvailable { get; }
+
+        public bool IsFreelancer { get; }
+
+        public string CompanyName { get; }
+
+        public IReadOnlyList<string> Abilities { get; }
+
+        public int HiddenAbilitiesCount { get; }
+
+        public ImageSource Picture { get; }
     }
 }
