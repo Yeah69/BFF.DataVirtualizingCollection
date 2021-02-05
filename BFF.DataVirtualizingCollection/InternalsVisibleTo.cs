e9dde2c8774db23e1bb62a08b544b5ecb6bc4063
Dima Enns <dieter.enns@gmail.com>
2021-02-05 14:56:07 +0100

    Shortened names of peripheral projects

--- BFF.DataVirtualizingCollection/InternalsVisibleTo.cs
+++ BFF.DataVirtualizingCollection/InternalsVisibleTo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("BFF.DataVirtualizingCollection.UnitTests")]
+[assembly: InternalsVisibleTo("BFF.DataVirtualizingCollection.Tests.Unit")]
 [assembly: InternalsVisibleTo("BFF.DataVirtualizingCollection.IntegrationTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
